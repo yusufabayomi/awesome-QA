6deac7b028d8ed34b9c6d1b182cbc9809736cb81
Yomi <yomi@acclio.com>
2021-06-30 17:39:52 +0100

    Project Setup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './assets/style.css';
+function App() {
+  return (
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          Column
+        </div>
+        <div class="col">
+          Column
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
